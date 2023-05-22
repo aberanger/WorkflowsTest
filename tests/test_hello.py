af062699bb8db907e44cadce4296935e06d28396
Anna Béranger <anbberanger@gmail.com>
2023-05-22 15:33:28 +0200

    adds workflow_dispatch

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,7 +1,8 @@
 from hello import hello
 
 def test_hello():
-        assert hello() == "hello"
-        
+    assert hello() == "hello"
+
 def test_hello_uppercase():
     assert hello(True) == "HELLO"
+    
